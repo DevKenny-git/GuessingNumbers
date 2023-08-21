bdc396b541658e69cb7a422a857683e984196342
Shodipe Oladele <quickly2kenny@gmail.com>
2023-08-21 15:02:39 +0100

    first commit

---
+++ guess.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync') ({signit: true});
+
+let endPlay = false;
+
+while (!endPlay) {
+    const computerGuess = Math.floor(Math.random() * 10) + 1;
+    let playerGuess = prompt("Guess a number between 1 and 10: ");
+
+    if (computerGuess !== Number(playerGuess)) {
+        console.log(`Computer guessed ${computerGuess}, Player guessed ${playerGuess}`);
+        console.log("Try again");
+        // playerGuess = prompt("Guess a number between 1 and 10: ");
+        endPlay = false
+    } else if (computerGuess ===  Number(playerGuess)) {
+        console.log(`Computer guessed ${computerGuess}, Player guessed ${playerGuess}`);
+        console.log("You win");
+        endPlay = true;
+    }
+    
+}
