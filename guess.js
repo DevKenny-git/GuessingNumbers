4bc478c8d5c0967847bbfd68011c7b3f0c0b22fb
Shodipe Oladele <quickly2kenny@gmail.com>
2023-08-21 15:07:17 +0100

    Adjusted the message for win or loss

--- guess.js
+++ guess.js
@@ -8,12 +8,12 @@ while (!endPlay) {
 
     if (computerGuess !== Number(playerGuess)) {
         console.log(`Computer guessed ${computerGuess}, Player guessed ${playerGuess}`);
-        console.log("Try again");
+        console.log("You have lost!");
         // playerGuess = prompt("Guess a number between 1 and 10: ");
         endPlay = false
     } else if (computerGuess ===  Number(playerGuess)) {
         console.log(`Computer guessed ${computerGuess}, Player guessed ${playerGuess}`);
-        console.log("You win");
+        console.log("You have won!");
         endPlay = true;
     }
     
