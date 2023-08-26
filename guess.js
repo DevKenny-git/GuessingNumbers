8827c12201038d21f4e8e7c47788c6cffde8f3a8
Shodipe Oladele <quickly2kenny@gmail.com>
2023-08-26 21:58:43 +0100

    Implement exit so the game can run continuously until it is encountered

--- guess.js
+++ guess.js
@@ -5,16 +5,17 @@ let endPlay = false;
 while (!endPlay) {
     const computerGuess = Math.floor(Math.random() * 10) + 1;
     let playerGuess = prompt("Guess a number between 1 and 10: ");
-
-    if (computerGuess !== Number(playerGuess)) {
+    if (playerGuess == 'exit') {
+        break;
+    } else if (computerGuess !== Number(playerGuess)) {
         console.log(`Computer guessed ${computerGuess}, Player guessed ${playerGuess}`);
         console.log("You have lost!");
-        // playerGuess = prompt("Guess a number between 1 and 10: ");
+        
         endPlay = false
     } else if (computerGuess ===  Number(playerGuess)) {
         console.log(`Computer guessed ${computerGuess}, Player guessed ${playerGuess}`);
         console.log("You have won!");
-        endPlay = true;
+        endPlay = false;
     }
     
 }
